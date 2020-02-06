43975d2d04d7f32912d1b25cd99d4710948be145
Huras Alexandre Ferreira dos Santos Gomes <hurat@gmail.com>
2020-02-06 12:16:27 -0200

    Projects + Workdays + Projects and Workdays relationships

---
+++ app/Models/Workday.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Workday extends Model
+{
+    protected $table = 'workdays';
+
+    public function projects()
+    {
+        return $this->belongsToMany('App\Models\Project');
+    }
+}
