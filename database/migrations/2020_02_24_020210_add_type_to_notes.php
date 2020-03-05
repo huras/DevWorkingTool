ef1d5dadf386a7a2cab076b24ea504e86f012e59
Huras Alexandre Ferreira dos Santos Gomes <hurat@gmail.com>
2020-03-05 10:28:38 -0300

    feat: update migration note type

--- database/migrations/2020_02_24_020210_add_type_to_notes.php
+++ database/migrations/2020_02_24_020210_add_type_to_notes.php
@@ -14,7 +14,7 @@ class AddTypeToNotes extends Migration
     public function up()
     {
         Schema::table('notes', function (Blueprint $table) {
-            $table->string('type')->default('text');
+            $table->string('type')->default('text')->nullable();
         });
     }
 
